8530a600825fa74e410d45c688340b878b507c24
𝙎𝙪𝙣𝙞𝙡 𝙎𝙝𝙖𝙧𝙢𝙖 <ji2796709@gmail.com>
2025-10-26 22:19:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import QuizApp from './components/QuizApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <QuizApp />
+    </div>
+  );
+}
+
+export default App;
